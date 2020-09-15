e7928d6a09bda366bc05c384902c5887eaa3b7be
adhish-bahl <adhishbahl0@gmail.com>
2020-09-15 22:51:03 +0530

    Third Commit

--- javas.js
+++ javas.js
@@ -1,4 +1,14 @@
-redir()
+function check1()
 {
-    
+    alert("sucess");
+}
+function big()
+{
+    document.getElementById("butt").style.width="88px";
+    document.getElementById("butt").style.height="33px";
+}
+function small()
+{
+    document.getElementById("butt").style.width="80px";
+    document.getElementById("butt").style.height="25px";
 }
