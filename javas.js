19a6ccfd46fcb07f9b20c5c0793edf75ffe0f524
adhish-bahl <adhishbahl0@gmail.com>
2020-09-16 01:25:32 +0530

    !5-09 Finish

--- javas.js
+++ javas.js
@@ -1,6 +1,33 @@
 function check1()
 {
-    alert("sucess");
+    var email1 = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+    var passle = document.getElementsByClassName("pass").length;
+    if(email1=="")
+    {
+        alert("E-mail I'd can not be left blank");
+    }
+    else if(pass.length<8)
+    {
+        alert("Password too short. Password must atleast be 8 digits long.");
+    }
+    else if(pass=="")
+    {
+        alert("Password can not be left blank.");
+    }
+    else if(pass==" ")
+    {
+        alert("Password can not start with space.")
+    }
+    else
+    {
+        redi();
+        alert("Log-in sucessfull!");
+    }
+}
+function redi()
+{
+    window.location = "https://christuniversity.in/";
 }
 function big()
 {
