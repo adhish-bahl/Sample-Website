0967ffcb9fd668d16e795e1915ceb1476662946c
adhish-bahl <adhishbahl0@gmail.com>
2020-09-16 17:23:53 +0530

    Almost Done

--- javas.js
+++ javas.js
@@ -1,39 +1,56 @@
 function check1()
 {
-    var email1 = document.getElementById("email").value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("sname").value;
+    var email = document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
-    var passle = document.getElementsByClassName("pass").length;
-    if(email1=="")
+    var cpass = document.getElementById("cpass").value;
+    var num = document.getElementById("num").value;
+    if(fname=="" || fname==" ")
     {
-        alert("E-mail I'd can not be left blank");
+        alert("First Name can not be left blank.");
     }
-    else if(pass.length<8)
+    else if(lname=="" || lname==" ")
     {
-        alert("Password too short. Password must atleast be 8 digits long.");
+        alert("Last Name can not be left blank.");
+    }
+    else if(email=="" || email==" ")
+    {
+        alert("E-mail I'd can not be left blank.");
     }
     else if(pass=="")
     {
         alert("Password can not be left blank.");
     }
-    else if(pass==" ")
+    else if(pass.length<8)
     {
-        alert("Password can not start with space.")
+        alert("Password too short. Password must atleast be 8 digits long.");
+    }
+    else if(cpass!=pass)
+    {
+        alert("Passwords dosen't match!");
+    }
+    else if(num=="" || num==" ")
+    {
+        alert("Number can not be left blank");
+    }
+    else if(num.length!=10)
+    {
+        alert("Enter valid 10 digit Number.");
     }
     else
     {
-        redi();
-        alert("Log-in sucessfull!");
+        window.location = "https://christuniversity.in/";
+        alert("Signed-in sucessfull!");
     }
 }
-function redi()
-{
-    window.location = "https://christuniversity.in/";
-}
+
 function big()
 {
     document.getElementById("butt").style.width="88px";
     document.getElementById("butt").style.height="33px";
 }
+
 function small()
 {
     document.getElementById("butt").style.width="80px";
